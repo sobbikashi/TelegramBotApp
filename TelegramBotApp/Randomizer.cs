169cc0dadb48ca79f28b4c065e00418f5a5fcf76
Константин Груздев <vanilmirth@inbox.ru>
2023-08-09 22:31:29 +0300

    рандомайзер

---
+++ TelegramBotApp/Randomizer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramBotApp
+{
+    public class Randomizer
+    {
+       
+
+        public int GetRandom(Random random)
+        {
+            int result;
+            return (result = random.Next(0, 100));
+        }
+    }
+}
